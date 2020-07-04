7b1928271c4feca5c3e37d59f86a60b1a1f6f10b
subhamshandilya <64160068+subhamshandilya@users.noreply.github.com>
2020-07-04 23:17:07 +0530

    Api-Gateway

---
+++ cloud-gateway/src/main/java/com/javatechie/cloud/gateway/FallbackController.java
@@ -0,0 +1,23 @@
+package com.javatechie.cloud.gateway;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class FallbackController {
+
+@RequestMapping("/orderFallBack")
+    public Mono<String> orderServiceFallBack(){
+    return Mono.just("Order service is taking too long  to respond or is down");
+
+
+    }
+    @RequestMapping("/paymentFallBack")
+    public Mono<String> paymentServiceFallBack(){
+        return Mono.just("Payment service is taking too long  to respond or is down");
+
+
+    }
+
+}
